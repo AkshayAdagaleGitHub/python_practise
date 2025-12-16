aec8febe01a223670a7fca75e76014032da3c203
Akshay A <akshayadagale@gmail.com>
2025-12-16 09:41:02 +0530

    number finder

---
+++ number_finder.py
@@ -0,0 +1,23 @@
+
+from typing import List
+
+def find_first(array: List[int], num:int) -> int:
+    left = 0
+    right = len(array) - 1
+    ans = -1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if array[mid] == num:
+            ans = mid
+            right = mid - 1
+        elif array[mid] < num:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return ans
+
+print(find_first([200, 200, 200, 200, 500, 500, 500], 200))
+print(find_first([200, 200, 200, 200, 500, 500, 500], 500))
+print(find_first([200, 200, 200, 200, 500, 500, 500], 100))
