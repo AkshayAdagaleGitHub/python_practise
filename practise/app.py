ff8dbc6496d72016d6626a4a546227db452f610c
Akshay A <akshayadagale@gmail.com>
2026-01-08 22:42:27 +0530

    adding new practise folder

---
+++ practise/app.py
@@ -0,0 +1,23 @@
+print('My name is Akshay, my age is', 34)
+print('Welcome')
+
+# Variables in Python
+
+name = 'Akshay'
+age = 18
+print(name + ' is a boy')
+print(name + ' is a boy and his age is ',age)
+print(name)
+
+# new line and break
+print('Hello World !!\nHow are you ?')
+print(name[0].lower())
+print(name[0].upper())
+# Length of a string
+print(len(name))
+# Get index of a string
+print(name.index('h'))
+# Replace
+print(name.replace('h','H'))
+
+# Number in Python
